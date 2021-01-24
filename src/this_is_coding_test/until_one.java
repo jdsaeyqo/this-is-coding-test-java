2fc93c7e34b06e5ca472d268d8defe961b4227f3
jdsaeyqo <jdsaeyqi@naver.com>
2021-01-24 13:18:26 +0900

    1이 될 때까지

---
+++ src/this_is_coding_test/until_one.java
@@ -0,0 +1,27 @@
+package this_is_coding_test;
+
+import java.util.Scanner;
+
+public class until_one {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int cnt = 0;
+		
+		int num = sc.nextInt();
+		int num1 = sc.nextInt();
+		
+		
+		while(num != 1) {
+			if(num % num1 == 0) {
+				num /=num1;
+				cnt++;
+			}else {
+				num-=1;
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+		
+		}
+}
