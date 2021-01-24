17b66421d3b9a82028e2c15287d6635e7822627b
jdsaeyqo <jdsaeyqi@naver.com>
2021-01-24 13:15:38 +0900

    카드 숫자게임

---
+++ src/this_is_coding_test/card__number_game.java
@@ -0,0 +1,35 @@
+package this_is_coding_test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class card__number_game {
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner(System.in);
+	
+	int max = 0;
+	
+	int row = sc.nextInt();
+	int col = sc.nextInt();
+	
+	int[] min_arr = new int[col];
+	int[] max_arr = new int[row];
+	
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			int num = sc.nextInt();
+			min_arr[j] = num;
+		}
+		Arrays.sort(min_arr);
+		max_arr[i]=min_arr[0];
+		
+	}
+	Arrays.sort(max_arr);
+	max = max_arr[row-1];
+	System.out.println(max);
+	
+	
+	
+	}
+}
