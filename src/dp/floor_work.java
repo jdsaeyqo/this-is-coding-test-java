cdf328c6a0ce9a4344e40aeebd134895e1dff622
jdsaeyqo <jdsaeyqi@naver.com>
2021-02-02 16:51:55 +0900

    바닥 공사

---
+++ src/dp/floor_work.java
@@ -0,0 +1,33 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class floor_work {
+public static void main(String[] args) throws IOException {
+	
+	
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st = new StringTokenizer(br.readLine());
+	
+	int n = Integer.parseInt(st.nextToken());
+	
+	int[] sum = new int[n+1];
+	
+	sum[1] = 1;
+	sum[2] = 3;
+	for (int i = 3; i < sum.length; i++) {
+		sum[i] = (sum[i-1] + 2*sum[i-2]) % 796796;
+	}
+	
+	System.out.println(sum[n]);
+	
+	
+	
+
+			
+}
+}
